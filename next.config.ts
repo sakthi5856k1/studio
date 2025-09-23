d2367bdd45701d4cf2cc78bd4c105565aa12bb09
Play With Sakthi Gaming <playwithsakthigaming@gmail.com>
2025-09-23 23:54:56 +0000

    Error: Invalid src prop (https://cdn.discordapp.com/attachments/12815511

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'media.discordapp.net',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
