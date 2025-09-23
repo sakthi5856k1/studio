3e4d6db4003fdb3cea61066b778f429aff23f52d
Play With Sakthi Gaming <playwithsakthigaming@gmail.com>
2025-09-23 12:07:54 +0000

    add tamil pasanga rgb color running gif effect

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,15 +89,14 @@ export default {
           },
         },
         'rgb-text': {
-          '0%': { 'background-position': '0% 50%' },
-          '50%': { 'background-position': '100% 50%' },
-          '100%': { 'background-position': '0% 50%' },
+          '0%': { 'background-position': '200% 0' },
+          '100%': { 'background-position': '-200% 0' },
         },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'rgb-text': 'rgb-text 6s ease-in-out infinite',
+        'rgb-text': 'rgb-text 6s linear infinite',
       },
     },
   },
