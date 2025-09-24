c983943b011f6dffde758a85d62a453388a3d5cc
Play With Sakthi Gaming <playwithsakthigaming@gmail.com>
2025-09-24 09:29:53 +0000

    change chirtmas cap

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -52,7 +52,7 @@ const SteamIcon = (props: React.SVGProps<SVGSVGElement>) => (
 
 const ChristmasCap = () => (
   <Image
-    src="https://skit.ng/wp-content/uploads/2023/12/Christmas-Hats-Skit-Store.webp"
+    src="https://media.discordapp.net/attachments/1116720480544636999/1274425873201631304/TP_NEW_WB_PNGxxxhdpi.png?ex=68d4d8d5&is=68d38755&hm=b6d4e0e4ef2c3215a4de4fb2f592189a60ddd94c651f96fe04deac2e7f96ddc6&=&format=webp&quality=lossless&width=826&height=826"
     alt="Christmas Cap"
     width={60}
     height={60}
